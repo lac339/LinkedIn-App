ab48f52c6bdb0fafcc49c0ff600e5abd45f588ad
lac339 <lac339@georgetown.edu>
2024-12-12 20:03:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
